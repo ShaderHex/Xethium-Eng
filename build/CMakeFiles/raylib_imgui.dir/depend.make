1f09851794859f481f5d67c91e1aff88486cbf9e
ShaderHex <barkott9@gmail.com>
2025-01-18 19:06:43 +0300

    Removed build folder

--- build/CMakeFiles/raylib_imgui.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for raylib_imgui.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/xethium.dir/src/main.cpp.o.d
+++
@@ -1,9 +0,0 @@
-CMakeFiles/xethium.dir/src/main.cpp.o: \
- /home/ausr/Documents/Projects/xeth/xeth/src/main.cpp \
- /usr/include/stdc-predef.h \
- /home/ausr/Documents/Projects/xeth/xeth/src/Core/Application.h \
- /home/ausr/Documents/Projects/xeth/xeth/src/Core/Renderer.h \
- /home/ausr/Documents/Projects/xeth/xeth/external/raylib/src/raylib.h \
- /usr/lib/gcc/x86_64-redhat-linux/14/include/stdarg.h \
- /home/ausr/Documents/Projects/xeth/xeth/external/rlimgui/rlImGui.h \
- /home/ausr/Documents/Projects/xeth/xeth/external/rlimgui/extras/IconsFontAwesome6.h
